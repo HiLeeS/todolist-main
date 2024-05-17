8ede433e9a360f3474646129494939609068f3e4
이승준 <72748734+HiLeeS@users.noreply.github.com>
2024-05-17 15:11:20 +0900

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'todolist',
         'USER': 'root',
-        'PASSWORD': 'qorenehf1',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
